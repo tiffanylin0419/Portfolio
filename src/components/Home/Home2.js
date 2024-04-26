906d27c37bf4881a16997207b4becca836abd0ae
Tiffany <tl330@duke.edu>
2024-04-26 02:49:59 -0400

    personalize home page

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -4,7 +4,6 @@ import myImg from "../../Assets/avatar.svg";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
-  AiOutlineTwitter,
   AiFillInstagram,
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
@@ -19,36 +18,27 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              I'm passionate about the world of technology and its power to bring ideas to life. 
+              Whether it's developing innovative software solutions, 
+              contributing to open-source projects, or simply sharing knowledge with others, 
+              I'm constantly seeking opportunities to grow as a developer and make a difference in the tech community.
               <br />
-              <br />I am fluent in classics like
+              <br />I am fluent in backend languages like
               <i>
-                <b className="purple"> C++, Javascript and Go. </b>
+                <b className="purple"> Python, Java and C++. </b>
               </i>
-              <br />
-              <br />
-              My field of Interest's are building new &nbsp;
+              I have also had some experiences in frontend development using languages like
               <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
-                <b className="purple">
-                  Blockchain.
-                </b>
+                <b className="purple"> JavaScript and TypeScript. </b>
               </i>
+              I also apply my passion for developing products
+              with <b className="purple">Node.js, Express.js and React.js</b>.
               <br />
               <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
+              My field of Interest's are 
               <i>
-                <b className="purple">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js and Next.js</b>
+                <b className="purple"> developing algorithms </b> and working on 
+                <b className="purple"> fullstack projects </b>.
               </i>
             </p>
           </Col>
@@ -61,13 +51,10 @@ function Home2() {
         <Row>
           <Col md={12} className="home-about-social">
             <h1>FIND ME ON</h1>
-            <p>
-              Feel free to <span className="purple">connect </span>with me
-            </p>
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/soumyajit4419"
+                  href="https://github.com/tiffanylin0419"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -77,17 +64,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://twitter.com/Soumyajit4419"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <AiOutlineTwitter />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://www.linkedin.com/in/soumyajit4419/"
+                  href="https://www.linkedin.com/in/tiffany-lin-0419t/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -97,7 +74,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/soumyajit4419"
+                  href="https://www.instagram.com/ting_hsuan0419"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
@@ -106,6 +83,9 @@ function Home2() {
                 </a>
               </li>
             </ul>
+            <p>
+              Feel free to <span className="purple">connect </span>with me
+            </p>
           </Col>
         </Row>
       </Container>
